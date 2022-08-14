410bcb75201e30a4cfa2a6ee768706253f2783be
Ronit Tushir <ronit.tushir@appinventiv.com>
2022-08-15 02:36:20 +0530

    more refactoring

--- Kudu 5.10.59 AM/Kudu/Modules/Home/GuestProfileVC/GuestProfileVC.swift
+++ Kudu 5.10.59 AM/Kudu/Modules/Home/GuestProfileVC/GuestProfileVC.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import LanguageManager_iOS
 
 class GuestProfileVC: SideMenuVC {
     
@@ -95,42 +94,7 @@ extension GuestProfileVC: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         guard let row = Rows(rawValue: indexPath.row) else { return }
         if row == .menu {
-            let currentDeliveryLocation = DataManager.shared.currentDeliveryLocation
-            WebServices.HomeEndPoints.getMenuList(request: MenuListRequest(servicesType: .delivery, lat: currentDeliveryLocation?.latitude, long: currentDeliveryLocation?.longitude, storeId: nil), success: { [weak self] (response) in
-                guard let strongSelf = self else { return }
-                var categories = response.data ?? []
-                if categories.isEmpty { return }
-                categories[0].isSelectedInApp = true
-                let vc = ExploreMenuVC.instantiate(fromAppStoryboard: .Home)
-                vc.viewModel = ExploreMenuVM(menuCategories: categories, delegate: vc)
-                mainThread {
-                    strongSelf.dismissSideMenu(pushViewController: vc)
-                }
-            }, failure: { [weak self] (error) in
-                guard let strongSelf = self else { return }
-                mainThread {
-                    let errorToast = AppErrorToastView(frame: CGRect(x: 0, y: 0, width: strongSelf.view.width - 32, height: 48))
-                    errorToast.show(message: error.msg, view: strongSelf.view)
-                }
-            })
+            self.triggerMenuFlow()
         }
-    }
-}
-
-extension GuestProfileVC {
-    func changeLanguage(toArabic isArabic: Bool) {
-        self.view.isUserInteractionEnabled = false
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
-            let language: AppUserDefaults.Language = isArabic ? .ar : .en
-            AppUserDefaults.save(value: language.rawValue, forKey: .selectedLanguage)
-            LanguageManager.shared.setLanguage(language: isArabic ? .ar : .en) { _ -> UIViewController in
-                let homeVC = HomeVC.instantiate(fromAppStoryboard: .Home)
-                homeVC.viewModel = HomeVM(delegate: homeVC)
-                let navVC = BaseNavVC(rootViewController: homeVC)
-                return navVC
-            } animation: { view in
-                view.transform = CGAffineTransform(translationX: 0, y: UIScreen.main.bounds.height)
-            }
-        })
     }
 }
